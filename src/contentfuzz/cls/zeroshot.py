b710ead6725ca4e18d349ed396ae584f1de5afcd
Yifeng He <yfhe.prsn@gmail.com>
2025-08-24 18:30:51 -0700

    fix: model name

--- src/contentfuzz/cls/zeroshot.py
+++ src/contentfuzz/cls/zeroshot.py
@@ -29,7 +29,7 @@ class OpenAIAnalyzer:
         """Using OpenAI API to analyze the stance of a given text"""
 
         completion = self.client.beta.chat.completions.parse(
-            model="gpt-4o-2024-08-06",
+            model=self.model,
             messages=[
                 {
                     "role": "system",
