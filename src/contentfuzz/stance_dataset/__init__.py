e81a318d23324a804a1de93bd3f1fc19f7e4a089
Yifeng He <yfhe.prsn@gmail.com>
2025-11-09 15:01:19 -0800

    add sem16 in running pipeline

--- src/contentfuzz/stance_dataset/__init__.py
+++ src/contentfuzz/stance_dataset/__init__.py
@@ -1,12 +1,15 @@
+from typing import Literal
 from .utils import negate_stance
 from .c_stance import load_c_stance, CSTANCEChoices
+from .sem16 import load_sem16
 from ._base import StanceDataEntry, StanceDataset
 
-Dataset = CSTANCEChoices  # todo: add more datasets
+Dataset = Literal[CSTANCEChoices, "sem16"]  # todo: add more datasets
 
 __all__ = [
     "negate_stance",
     "load_c_stance",
+    "load_sem16",
     "CSTANCEChoices",
     "Dataset",
     "StanceDataEntry",
