ae1e212c5064b226e14b26e762ec04e3789b68f6
Yifeng He <yfhe.prsn@gmail.com>
2025-11-10 21:27:26 -0800

    add fuzz roberta sem16 script

--- experiments/sem16_sched.sh
+++ experiments/sem16_sched.sh
@@ -6,12 +6,22 @@ schedules=("fifo" "priority" "random" "priority_random")
 
 for schedule in "${schedules[@]}"; do
     echo "Running schedule: ${schedule}"
+    # uv run src/run_fuzz.py \
+    #     sem16 \
+    #     zeroshot \
+    #     results/zeroshot+gemini-2.5-flash-lite+sem16.jsonl \
+    #     --cls-model gemini-2.5-flash-lite \
+    #     --fuzzer-model gemini-2.5-flash-lite \
+    #     --sample-n 100 \
+    #     --schedule "${schedule}"
+
     uv run src/run_fuzz.py \
         sem16 \
-        zeroshot \
-        results/zeroshot+gemini-2.5-flash-lite+sem16.jsonl \
-        --cls-model gemini-2.5-flash-lite \
+        encoder \
+        results/encoder+saved_models--FacebookAI--roberta-base+sem16--+sem16.jsonl \
+        --cls-model saved_models/google-bert/bert-base-cased+sem16 \
         --fuzzer-model gemini-2.5-flash-lite \
-        --sample-n 100 \
+        --sample-n 200 \
+        --n-iters 300 \
         --schedule "${schedule}"
 done
