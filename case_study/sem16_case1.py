2f956f029779a8b17274062414eda392a6eee9b2
Yifeng He <yfhe@ucdavis.edu>
2025-11-28 21:33:51 -0800

    add PRINT_SEEDS model for case study

---
+++ case_study/sem16_case1.py
@@ -0,0 +1,36 @@
+import orjson
+from returns.result import Success, Failure
+from contentfuzz.fuzz import Fuzzer, Mutator
+from contentfuzz.fuzz.seed_scheduler import PriorityScheduler
+from contentfuzz.stance_dataset import StanceDataEntry
+from contentfuzz.cls import Encoder
+
+
+def main():
+    """reproduce a simple fuzzing run for case study"""
+    task: StanceDataEntry = {
+        "stance": "Against",
+        "target": "Atheism",
+        "text": "I am human. I look forward to the extinction of humanity with eager anticipation. We deserve nothing less.",
+    }
+    mutator = Mutator()
+    model_path = "saved_models/FacebookAI/roberta-base+sem16"
+    analyzer = Encoder(model_path)
+    scheduler = PriorityScheduler()
+    fuzzer = Fuzzer(analyzer, mutator, scheduler)
+    match fuzzer.runs(task, n_iters=10):
+        case Success(((mutated_text, stance, confidence), iter_cnt)):
+            log_obj = task | {
+                "new_text": mutated_text,
+                "predicted": stance,
+                "confidence": confidence,
+                "iteration": iter_cnt,
+            }
+            print("Fuzzing succeeded:")
+            print(orjson.dumps(log_obj, option=orjson.OPT_INDENT_2).decode())
+        case Failure(e):
+            print(f"Fuzzing failed: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/contentfuzz/env.py
@@ -0,0 +1,3 @@
+import os
+
+PRINT_SEEDS = bool(os.getenv("PRINT_SEEDS", "0"))
