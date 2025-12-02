912013dabd9e21169138cae7a5610a79a8f94779
Yifeng He <yfhe@ucdavis.edu>
2025-12-02 05:56:09 +0000

    add error message when no seed

--- src/contentfuzz/fuzz/fuzzer.py
+++ src/contentfuzz/fuzz/fuzzer.py
@@ -123,8 +123,9 @@ class Fuzzer:
                 case Success(seed):
                     return Success((seed, i))
                 case Failure(IndexError()):  # no more seeds to pick
-                    assert len(self.scheduler.population) == 0
-                    break
+                    if len(self.scheduler.population) == 0:
+                        errs.append(f"{i}: No more seeds to pick from.")
+                        break
                 case Failure(e):
                     errs.append(f"{i}: {str(e)}")
         return Failure(errs)
