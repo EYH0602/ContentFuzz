a2e5db78ef4b7069c925e5ee910eaa14789be8d2
Yifeng He <yfhe@ucdavis.edu>
2025-12-17 16:02:00 +0000

    experiment: change model path

--- experiments/fuzz_bert_sem16.sh
+++ experiments/fuzz_bert_sem16.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 uv run src/run_fuzz.py \
     sem16 \
     encoder \
-    results/encoder+saved_models--google-bert--bert-base-cased--sem16+sem16.jsonl \
-    --cls-model saved_models/google-bert/bert-base-cased/sem16 \
+    results/encoder+saved_models--google-bert--bert-base-uncased--sem16+sem16.jsonl \
+    --cls-model saved_models/google-bert/bert-base-uncased/sem16 \
     --fuzzer-model gemini-2.5-flash-lite \
     --schedule priority \
     --n-iters 300
